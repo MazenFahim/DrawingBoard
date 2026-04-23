1963a4cccdfe5d4b90fe2893aa9074177df0fe08
Mazen Ashraf <mezoashee@gmail.com>
2026-04-23 14:42:34 +0200

    added icon

--- src/main/java/com/mazenfahim/drawingboard/DrawingApplication.java
+++ src/main/java/com/mazenfahim/drawingboard/DrawingApplication.java
@@ -3,16 +3,26 @@ package com.mazenfahim.drawingboard;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class DrawingApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(DrawingApplication.class.getResource("main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 860, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("EasyBrush");
+        Image icon = new Image(
+                Objects.requireNonNull(
+                        DrawingApplication.class.getResourceAsStream(
+                                "icons/EasyBrush1.png"
+                        )
+                )
+        );
+        stage.getIcons().add(icon);
         stage.setScene(scene);
         stage.show();
     }
